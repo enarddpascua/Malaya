d59ad5e4d7fb96b14b179598c3ae72ce8c13fd88
enard <enarddpascua@gmail.com>
2022-06-13 10:45:30 +1000

    home page with boilerplates

---
+++ malaya/pages/index.js
@@ -0,0 +1,10 @@
+import Layout from '@/components/Layout'
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1>Home</h1>
+      {/* <Link href="/about">About</Link> */}
+    </Layout>
+  )
+}
